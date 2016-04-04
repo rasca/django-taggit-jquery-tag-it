366d5b59c33988c51b8049237c840ea84f4b70ff
Simon Litchfield <simon@s29.com.au>
2016-04-04 15:47:21 +1000

    Tag autocomplete endpoint shouldn't be staff-only by default

--- taggit_autocomplete/urls.py
+++ taggit_autocomplete/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import *
-from django.contrib.admin.views.decorators import staff_member_required
-
 from taggit_autocomplete.views import list_tags
 
 urlpatterns = [
-    url(r'^list/$', staff_member_required(list_tags),
-        name='taggit_autocomplete-list'),
+    url(r'^list/$', list_tags, name='taggit_autocomplete-list'),
 ]
